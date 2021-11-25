34ceddefec53fd835f3bbf47698c20c17c3ab5d2
Ola Vish <olgavish1@gmail.com>
2021-11-25 17:59:50 +0200

    Added vgg loading function

--- project/models/segnet.py
+++ project/models/segnet.py
@@ -2,6 +2,7 @@ import torch
 import torchvision
 import torch.nn as nn
 
+
 class VggSubBlock(nn.Module):
     def __init__(self, input_channels, output_channels):
         super().__init__()
@@ -19,7 +20,7 @@ class VggBlock(nn.Module):
         super().__init__()
         self.first_block = VggSubBlock(input_channels, output_channels)
         self.remaining_blocks = nn.Sequential(
-            *[VggSubBlock(output_channels, output_channels) for idx in range(1, repetitions)])
+            *[VggSubBlock(output_channels, output_channels) for _ in range(1, repetitions)])
 
     def forward(self, x):
         x = self.first_block(x)
@@ -31,7 +32,7 @@ class DecoderBlock(nn.Module):
     def __init__(self, input_channels, output_channels, repetitions=2):
         super().__init__()
         self.first_blocks = nn.Sequential(
-            *[VggSubBlock(input_channels, input_channels) for idx in range(1, repetitions)])
+            *[VggSubBlock(input_channels, input_channels) for _ in range(1, repetitions)])
         self.last_block = VggSubBlock(input_channels, output_channels)
 
     def forward(self, x):
@@ -63,14 +64,14 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, channels):
         super().__init__()
-        self.max_uppool = nn.MaxUnpool2d(kernel_size=2, stride=2, padding=0)
+        self.max_unpool = nn.MaxUnpool2d(kernel_size=2, stride=2, padding=0)
         self.decoder_blocks = nn.ModuleList(
             [DecoderBlock(channels[idx - 1], channels[idx], repetitions=2) if idx > 3 else DecoderBlock(
                 channels[idx - 1], channels[idx], repetitions=3) for idx in range(1, len(channels))])
 
     def forward(self, x, pool_indices):
         for idx, block in enumerate(self.decoder_blocks):
-            x = self.max_uppool(x, pool_indices[idx])
+            x = self.max_unpool(x, pool_indices[idx])
             x = block(x)
         return x
 
@@ -78,7 +79,6 @@ class Decoder(nn.Module):
 class SegNet(nn.Module):
     def __init__(self, enc_chs, dec_chs, num_classes=2, warm_start=True):
         super().__init__()
-        # dec_chs.append(num_classes)
         self.encoder = Encoder(channels=enc_chs)
         self.decoder = Decoder(channels=dec_chs)
         self.last = VggSubBlock(dec_chs[-1], dec_chs[-1])
@@ -87,7 +87,7 @@ class SegNet(nn.Module):
             nn.BatchNorm2d(num_classes, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
         )
         if warm_start:
-            load_vgg_weights_to_encoder(self.encoder)
+            self.load_vgg_weights_to_encoder()
 
     def forward(self, x):
         encoder_features, pool_indices = self.encoder(x)
@@ -96,3 +96,15 @@ class SegNet(nn.Module):
         decoder_output = self.last(decoder_output)
         decoder_output = self.output(decoder_output)
         return decoder_output
+
+    def load_vgg_weights_to_encoder(self):
+        encoder_state_dict = self.encoder.state_dict()
+        encoder_keys = list(encoder_state_dict.keys())
+        vgg16 = torchvision.models.vgg16_bn(pretrained=True)
+        vgg_state_dict = vgg16.state_dict()
+        for idx, key in enumerate(vgg_state_dict):
+            if idx < len(encoder_keys):
+                curr_key = encoder_keys[idx]
+                curr_value = vgg_state_dict[key]
+                encoder_state_dict[curr_key] = curr_value
+        self.encoder.load_state_dict(encoder_state_dict)
